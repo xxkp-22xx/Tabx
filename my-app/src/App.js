ab06692a2140ec4c1e8c875d190703663de664c2
xxkp-22xx <khwaeeshpatel007@gmail.com>
2025-02-05 00:10:33 -0600

    React app code added

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Blockchain Based Expense Sharing App
+      </h1>
+      <h2>
+        Home Page
+      </h2>
+    </div>
+  );
+}
+
+export default App;
